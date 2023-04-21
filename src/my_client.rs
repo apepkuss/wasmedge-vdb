765ef7522e136294f12533168147c1eace663d97
Xin Liu <sam@secondstate.io>
2023-04-21 15:18:37 +0800

    chore: update new client and collection
    Signed-off-by: Xin Liu <sam@secondstate.io>

--- src/my_client.rs
+++ src/my_client.rs
@@ -1,6 +1,6 @@
 use base64::engine::general_purpose;
 use base64::Engine;
-use milvus::proto::common::{ConsistencyLevel, MsgType};
+use milvus::proto::common::{ConsistencyLevel, KeyValuePair, MsgType};
 use milvus::proto::milvus::milvus_service_client::MilvusServiceClient;
 use num_traits::FromPrimitive;
 use prost::{bytes::BytesMut, Message};
@@ -10,7 +10,10 @@ use tonic::transport::Channel;
 use tonic::Request;
 
 use crate::{
-    my_collection::{CollectionInfo, CollectionMetadata, PartitionInfo},
+    my_collection::{
+        CollectionInfo, CollectionMetadata, FieldData, IndexInfo, IndexProgress, IndexState,
+        MutationResult, PartitionInfo, SearchResult,
+    },
     my_error::{Error, Result},
     schema::CollectionSchema,
     utils::{new_msg, status_to_result},
@@ -481,6 +484,330 @@ impl Client {
                 in_memory_percentage: response.in_memory_percentages[i],
             });
         }
+
+        Ok(res)
+    }
+
+    pub async fn create_alias(&self, collection_name: &str, alias: &str) -> Result<()> {
+        let request = milvus::proto::milvus::CreateAliasRequest {
+            base: Some(new_msg(MsgType::CreateAlias)),
+            collection_name: collection_name.to_string(),
+            alias: alias.to_string(),
+            ..Default::default()
+        };
+
+        let status = self
+            .client
+            .clone()
+            .create_alias(request)
+            .await?
+            .into_inner();
+
+        status_to_result(&Some(status))
+    }
+
+    pub async fn drop_alias(&self, alias: &str) -> Result<()> {
+        let request = milvus::proto::milvus::DropAliasRequest {
+            base: Some(new_msg(MsgType::DropAlias)),
+            alias: alias.to_string(),
+            ..Default::default()
+        };
+
+        let status = self.client.clone().drop_alias(request).await?.into_inner();
+
+        status_to_result(&Some(status))
+    }
+
+    pub async fn alter_alias(&self, alias: &str, collection_name: &str) -> Result<()> {
+        let request = milvus::proto::milvus::AlterAliasRequest {
+            base: Some(new_msg(MsgType::AlterAlias)),
+            alias: alias.to_string(),
+            collection_name: collection_name.to_string(),
+            ..Default::default()
+        };
+
+        let status = self.client.clone().alter_alias(request).await?.into_inner();
+
+        status_to_result(&Some(status))
+    }
+
+    /// Create index for vector data
+    ///
+    /// # Arguments
+    ///
+    /// * `collection_name` - The name of the collection to create index.
+    ///
+    /// * `field_name` - The name of the vector field in the collection.
+    ///
+    ///
+    ///
+    /// * `index` - The index to create.
+    pub async fn create_index(
+        &self,
+        collection_name: &str,
+        field_name: &str,
+        extra_params: Option<HashMap<String, String>>,
+        index_name: &str,
+    ) -> Result<()> {
+        let request = milvus::proto::milvus::CreateIndexRequest {
+            base: Some(new_msg(MsgType::CreateIndex)),
+            collection_name: collection_name.to_string(),
+            field_name: field_name.to_string(),
+            extra_params: extra_params
+                .unwrap_or_default()
+                .iter()
+                .map(|(key, value)| KeyValuePair {
+                    key: key.clone(),
+                    value: value.clone(),
+                })
+                .collect(),
+            index_name: index_name.to_string(),
+            ..Default::default()
+        };
+
+        let status = self
+            .client
+            .clone()
+            .create_index(request)
+            .await?
+            .into_inner();
+
+        status_to_result(&Some(status))
+    }
+
+    pub async fn describe_index(
+        &self,
+        collection_name: &str,
+        field_name: &str,
+        index_name: &str,
+    ) -> Result<Vec<IndexInfo>> {
+        let request = milvus::proto::milvus::DescribeIndexRequest {
+            base: Some(new_msg(MsgType::DescribeIndex)),
+            collection_name: collection_name.to_string(),
+            field_name: field_name.to_string(),
+            index_name: index_name.to_string(),
+            ..Default::default()
+        };
+
+        let response = self
+            .client
+            .clone()
+            .describe_index(request)
+            .await?
+            .into_inner();
+
+        status_to_result(&response.status)?;
+
+        let mut res = vec![];
+        for i in 0..response.index_descriptions.len() {
+            res.push(IndexInfo {
+                index_name: response.index_descriptions[i].index_name.clone(),
+                index_id: response.index_descriptions[i].index_id,
+                params: response.index_descriptions[i]
+                    .params
+                    .iter()
+                    .map(|kv| (kv.key.clone(), kv.value.clone()))
+                    .collect(),
+                field_name: response.index_descriptions[i].field_name.clone(),
+                indexed_rows: response.index_descriptions[i].indexed_rows,
+                total_rows: response.index_descriptions[i].total_rows,
+                state: response.index_descriptions[i].state,
+                index_state_fail_reason: response.index_descriptions[i]
+                    .index_state_fail_reason
+                    .clone(),
+            });
+        }
+
+        Ok(res)
+    }
+
+    pub async fn get_index_state(
+        &self,
+        collection_name: &str,
+        field_name: &str,
+        index_name: &str,
+    ) -> Result<IndexState> {
+        let request = milvus::proto::milvus::GetIndexStateRequest {
+            base: Some(new_msg(MsgType::GetIndexState)),
+            collection_name: collection_name.to_string(),
+            field_name: field_name.to_string(),
+            index_name: index_name.to_string(),
+            ..Default::default()
+        };
+
+        let response = self
+            .client
+            .clone()
+            .get_index_state(request)
+            .await?
+            .into_inner();
+
+        status_to_result(&response.status)?;
+
+        Ok(IndexState {
+            state: response.state,
+            fail_reason: response.fail_reason,
+        })
+    }
+
+    pub async fn get_index_build_progress(
+        &self,
+        collection_name: &str,
+        field_name: &str,
+        index_name: &str,
+    ) -> Result<IndexProgress> {
+        let request = milvus::proto::milvus::GetIndexBuildProgressRequest {
+            base: Some(new_msg(MsgType::GetIndexBuildProgress)),
+            collection_name: collection_name.to_string(),
+            field_name: field_name.to_string(),
+            index_name: index_name.to_string(),
+            ..Default::default()
+        };
+
+        let response = self
+            .client
+            .clone()
+            .get_index_build_progress(request)
+            .await?
+            .into_inner();
+
+        status_to_result(&response.status)?;
+
+        Ok(IndexProgress {
+            total_rows: response.total_rows,
+            indexed_rows: response.indexed_rows,
+        })
+    }
+
+    pub async fn drop_index(
+        &self,
+        collection_name: &str,
+        field_name: &str,
+        index_name: &str,
+    ) -> Result<()> {
+        let request = milvus::proto::milvus::DropIndexRequest {
+            base: Some(new_msg(MsgType::DropIndex)),
+            collection_name: collection_name.to_string(),
+            field_name: field_name.to_string(),
+            index_name: index_name.to_string(),
+            ..Default::default()
+        };
+
+        let status = self.client.clone().drop_index(request).await?.into_inner();
+
+        status_to_result(&Some(status))
+    }
+
+    pub async fn insert(
+        &self,
+        collection_name: &str,
+        partition_name: &str,
+        fields_data: Vec<FieldData>,
+    ) -> Result<MutationResult> {
+        let request = milvus::proto::milvus::InsertRequest {
+            base: Some(new_msg(MsgType::Insert)),
+            collection_name: collection_name.to_string(),
+            partition_name: partition_name.to_string(),
+            num_rows: fields_data.len() as u32,
+            fields_data: fields_data
+                .into_iter()
+                .map(|field_data| field_data.into())
+                .collect(),
+            ..Default::default()
+        };
+
+        let response = self.client.clone().insert(request).await?.into_inner();
+
+        status_to_result(&response.status)?;
+
+        let res = MutationResult {
+            id: response.i_ds.map(|ids| ids.into()),
+            succ_index: response.succ_index,
+            err_index: response.err_index,
+            acknowledged: response.acknowledged,
+            insert_cnt: response.insert_cnt,
+            delete_cnt: response.delete_cnt,
+            upsert_cnt: response.upsert_cnt,
+            timestamp: response.timestamp,
+        };
+
+        Ok(res)
+    }
+
+    pub async fn delete(
+        &self,
+        collection_name: &str,
+        partition_name: &str,
+        expr: &str,
+    ) -> Result<MutationResult> {
+        let request = milvus::proto::milvus::DeleteRequest {
+            base: Some(new_msg(MsgType::Delete)),
+            collection_name: collection_name.to_string(),
+            partition_name: partition_name.to_string(),
+            expr: expr.to_string(),
+            ..Default::default()
+        };
+
+        let response = self.client.clone().delete(request).await?.into_inner();
+
+        status_to_result(&response.status)?;
+
+        let res = MutationResult {
+            id: response.i_ds.map(|ids| ids.into()),
+            succ_index: response.succ_index,
+            err_index: response.err_index,
+            acknowledged: response.acknowledged,
+            insert_cnt: response.insert_cnt,
+            delete_cnt: response.delete_cnt,
+            upsert_cnt: response.upsert_cnt,
+            timestamp: response.timestamp,
+        };
+
+        Ok(res)
+    }
+
+    pub async fn search(
+        &self,
+        collection_name: &str,
+        partition_names: Vec<&str>,
+        dsl: &str,
+        placeholder_group: Vec<u8>,
+        dsl_type: i32,
+        output_fields: Vec<String>,
+        search_params: HashMap<String, String>,
+        travel_timestamp: u64,
+        guarantee_timestamp: u64,
+        nq: i64,
+    ) -> Result<SearchResult> {
+        let request = milvus::proto::milvus::SearchRequest {
+            base: Some(new_msg(MsgType::Search)),
+            collection_name: collection_name.to_string(),
+            partition_names: partition_names.into_iter().map(|s| s.to_string()).collect(),
+            dsl: dsl.to_string(),
+            placeholder_group,
+            dsl_type,
+            output_fields,
+            search_params: search_params
+                .into_iter()
+                .map(|(k, v)| KeyValuePair {
+                    key: k.clone(),
+                    value: v.clone(),
+                })
+                .collect(),
+            travel_timestamp,
+            guarantee_timestamp,
+            nq,
+            ..Default::default()
+        };
+
+        let response = self.client.clone().search(request).await?.into_inner();
+
+        status_to_result(&response.status)?;
+
+        let res = SearchResult {
+            results: response.results.map(|x| x.into()),
+            collection_name: response.collection_name,
+        };
 
         Ok(res)
     }
--- src/my_collection.rs
+++ src/my_collection.rs
@@ -36,3 +36,320 @@ pub struct PartitionInfo {
     pub created_utc_timestamp: u64,
     pub in_memory_percentage: i64,
 }
+
+#[derive(Debug, Clone)]
+pub struct IndexInfo {
+    pub index_name: String,
+    pub index_id: i64,
+    pub params: std::collections::HashMap<String, String>,
+    pub field_name: String,
+    pub indexed_rows: i64,
+    pub total_rows: i64,
+    pub state: i32,
+    pub index_state_fail_reason: String,
+}
+
+#[derive(Debug, Clone)]
+pub struct IndexState {
+    pub state: i32,
+    pub fail_reason: String,
+}
+
+#[derive(Debug, Clone)]
+pub struct IndexProgress {
+    pub indexed_rows: i64,
+    pub total_rows: i64,
+}
+
+#[derive(Debug, Clone)]
+pub struct FieldData {
+    pub data_type: i32,
+    pub field_name: String,
+    pub field_id: i64,
+    pub field: Option<Field>,
+}
+impl From<FieldData> for milvus::proto::schema::FieldData {
+    fn from(field_data: FieldData) -> Self {
+        Self {
+            r#type: field_data.data_type,
+            field_name: field_data.field_name,
+            field_id: field_data.field_id,
+            field: field_data.field.map(|f| f.into()),
+        }
+    }
+}
+impl From<milvus::proto::schema::FieldData> for FieldData {
+    fn from(field_data: milvus::proto::schema::FieldData) -> Self {
+        Self {
+            data_type: field_data.r#type,
+            field_name: field_data.field_name,
+            field_id: field_data.field_id,
+            field: field_data.field.map(|f| f.into()),
+        }
+    }
+}
+
+#[derive(Debug, Clone)]
+pub enum Field {
+    Scalars(ScalarField),
+    Vectors(VectorField),
+}
+impl From<Field> for milvus::proto::schema::field_data::Field {
+    fn from(field: Field) -> Self {
+        match field {
+            Field::Scalars(scalar_field) => milvus::proto::schema::field_data::Field::Scalars(
+                milvus::proto::schema::ScalarField {
+                    data: scalar_field.data.map(|data| data.into()),
+                },
+            ),
+            Field::Vectors(vector_field) => milvus::proto::schema::field_data::Field::Vectors(
+                milvus::proto::schema::VectorField {
+                    dim: vector_field.dim,
+                    data: vector_field.data.map(|data| data.into()),
+                },
+            ),
+        }
+    }
+}
+impl From<milvus::proto::schema::field_data::Field> for Field {
+    fn from(field: milvus::proto::schema::field_data::Field) -> Self {
+        match field {
+            milvus::proto::schema::field_data::Field::Scalars(scalar_field) => {
+                Field::Scalars(scalar_field.into())
+            }
+            milvus::proto::schema::field_data::Field::Vectors(vector_field) => {
+                Field::Vectors(vector_field.into())
+            }
+        }
+    }
+}
+
+#[derive(Debug, Clone)]
+pub struct ScalarField {
+    pub data: Option<ScalarFieldData>,
+}
+impl From<ScalarField> for milvus::proto::schema::ScalarField {
+    fn from(field: ScalarField) -> Self {
+        milvus::proto::schema::ScalarField {
+            data: field.data.map(|data| data.into()),
+        }
+    }
+}
+impl From<milvus::proto::schema::ScalarField> for ScalarField {
+    fn from(field: milvus::proto::schema::ScalarField) -> Self {
+        Self {
+            data: field.data.map(|data| data.into()),
+        }
+    }
+}
+#[derive(Debug, Clone, PartialEq)]
+pub enum ScalarFieldData {
+    BoolData(Vec<bool>),
+    IntData(Vec<i32>),
+    LongData(Vec<i64>),
+    FloatData(Vec<f32>),
+    DoubleData(Vec<f64>),
+    StringData(Vec<String>),
+    BytesData(Vec<Vec<u8>>),
+}
+impl From<ScalarFieldData> for milvus::proto::schema::scalar_field::Data {
+    fn from(data: ScalarFieldData) -> Self {
+        match data {
+            ScalarFieldData::BoolData(v) => milvus::proto::schema::scalar_field::Data::BoolData({
+                milvus::proto::schema::BoolArray { data: v }
+            }),
+            ScalarFieldData::IntData(v) => milvus::proto::schema::scalar_field::Data::IntData(
+                milvus::proto::schema::IntArray { data: v },
+            ),
+            ScalarFieldData::LongData(v) => milvus::proto::schema::scalar_field::Data::LongData(
+                milvus::proto::schema::LongArray { data: v },
+            ),
+            ScalarFieldData::FloatData(v) => milvus::proto::schema::scalar_field::Data::FloatData(
+                milvus::proto::schema::FloatArray { data: v },
+            ),
+            ScalarFieldData::DoubleData(v) => {
+                milvus::proto::schema::scalar_field::Data::DoubleData(
+                    milvus::proto::schema::DoubleArray { data: v },
+                )
+            }
+            ScalarFieldData::StringData(v) => {
+                milvus::proto::schema::scalar_field::Data::StringData(
+                    milvus::proto::schema::StringArray { data: v },
+                )
+            }
+            ScalarFieldData::BytesData(v) => milvus::proto::schema::scalar_field::Data::BytesData(
+                milvus::proto::schema::BytesArray { data: v },
+            ),
+        }
+    }
+}
+impl From<milvus::proto::schema::scalar_field::Data> for ScalarFieldData {
+    fn from(data: milvus::proto::schema::scalar_field::Data) -> Self {
+        match data {
+            milvus::proto::schema::scalar_field::Data::BoolData(v) => {
+                ScalarFieldData::BoolData(v.data)
+            }
+            milvus::proto::schema::scalar_field::Data::IntData(v) => {
+                ScalarFieldData::IntData(v.data)
+            }
+            milvus::proto::schema::scalar_field::Data::LongData(v) => {
+                ScalarFieldData::LongData(v.data)
+            }
+            milvus::proto::schema::scalar_field::Data::FloatData(v) => {
+                ScalarFieldData::FloatData(v.data)
+            }
+            milvus::proto::schema::scalar_field::Data::DoubleData(v) => {
+                ScalarFieldData::DoubleData(v.data)
+            }
+            milvus::proto::schema::scalar_field::Data::StringData(v) => {
+                ScalarFieldData::StringData(v.data)
+            }
+            milvus::proto::schema::scalar_field::Data::BytesData(v) => {
+                ScalarFieldData::BytesData(v.data)
+            }
+        }
+    }
+}
+
+#[derive(Debug, Clone)]
+pub struct VectorField {
+    pub dim: i64,
+    pub data: Option<VectorFieldData>,
+}
+impl From<VectorField> for milvus::proto::schema::VectorField {
+    fn from(field: VectorField) -> Self {
+        milvus::proto::schema::VectorField {
+            dim: field.dim,
+            data: field.data.map(|data| data.into()),
+        }
+    }
+}
+impl From<milvus::proto::schema::VectorField> for VectorField {
+    fn from(field: milvus::proto::schema::VectorField) -> Self {
+        VectorField {
+            dim: field.dim,
+            data: field.data.map(|data| data.into()),
+        }
+    }
+}
+#[derive(Debug, Clone, PartialEq)]
+pub enum VectorFieldData {
+    BinaryVec(Vec<u8>),
+    FloatVec(Vec<f32>),
+}
+impl From<VectorFieldData> for milvus::proto::schema::vector_field::Data {
+    fn from(data: VectorFieldData) -> Self {
+        match data {
+            VectorFieldData::BinaryVec(v) => {
+                milvus::proto::schema::vector_field::Data::BinaryVector(v)
+            }
+            VectorFieldData::FloatVec(v) => milvus::proto::schema::vector_field::Data::FloatVector(
+                milvus::proto::schema::FloatArray { data: v },
+            ),
+        }
+    }
+}
+impl From<milvus::proto::schema::vector_field::Data> for VectorFieldData {
+    fn from(data: milvus::proto::schema::vector_field::Data) -> Self {
+        match data {
+            milvus::proto::schema::vector_field::Data::BinaryVector(v) => {
+                VectorFieldData::BinaryVec(v)
+            }
+            milvus::proto::schema::vector_field::Data::FloatVector(v) => {
+                VectorFieldData::FloatVec(v.data)
+            }
+        }
+    }
+}
+
+#[derive(Debug, Clone)]
+pub struct MutationResult {
+    pub id: Option<Id>,
+    pub succ_index: Vec<u32>,
+    pub err_index: Vec<u32>,
+    pub acknowledged: bool,
+    pub insert_cnt: i64,
+    pub delete_cnt: i64,
+    pub upsert_cnt: i64,
+    pub timestamp: u64,
+}
+
+#[derive(Debug, Clone)]
+pub struct Id {
+    id_field: Option<IdField>,
+}
+impl From<Id> for milvus::proto::schema::IDs {
+    fn from(id: Id) -> Self {
+        milvus::proto::schema::IDs {
+            id_field: id.id_field.map(|id_field| id_field.into()),
+        }
+    }
+}
+impl From<milvus::proto::schema::IDs> for Id {
+    fn from(ids: milvus::proto::schema::IDs) -> Self {
+        Id {
+            id_field: ids.id_field.map(|id_field| id_field.into()),
+        }
+    }
+}
+#[derive(Debug, Clone)]
+pub enum IdField {
+    IntId(Vec<i64>),
+    StrId(Vec<String>),
+}
+impl From<IdField> for milvus::proto::schema::i_ds::IdField {
+    fn from(id_field: IdField) -> Self {
+        match id_field {
+            IdField::IntId(v) => {
+                milvus::proto::schema::i_ds::IdField::IntId(milvus::proto::schema::LongArray {
+                    data: v,
+                })
+            }
+            IdField::StrId(v) => {
+                milvus::proto::schema::i_ds::IdField::StrId(milvus::proto::schema::StringArray {
+                    data: v,
+                })
+            }
+        }
+    }
+}
+impl From<milvus::proto::schema::i_ds::IdField> for IdField {
+    fn from(id_field: milvus::proto::schema::i_ds::IdField) -> Self {
+        match id_field {
+            milvus::proto::schema::i_ds::IdField::IntId(v) => IdField::IntId(v.data),
+            milvus::proto::schema::i_ds::IdField::StrId(v) => IdField::StrId(v.data),
+        }
+    }
+}
+
+#[derive(Debug, Clone)]
+pub struct SearchResult {
+    pub results: Option<SearchResultData>,
+    pub collection_name: String,
+}
+
+#[derive(Debug, Clone)]
+pub struct SearchResultData {
+    pub num_queries: i64,
+    pub top_k: i64,
+    pub fields_data: Vec<FieldData>,
+    pub scores: Vec<f32>,
+    pub id: Option<Id>,
+    pub topks: Vec<i64>,
+}
+impl From<milvus::proto::schema::SearchResultData> for SearchResultData {
+    fn from(data: milvus::proto::schema::SearchResultData) -> Self {
+        SearchResultData {
+            num_queries: data.num_queries,
+            top_k: data.top_k,
+            fields_data: data
+                .fields_data
+                .into_iter()
+                .map(|field_data| field_data.into())
+                .collect(),
+            scores: data.scores,
+            id: data.ids.map(|id| id.into()),
+            topks: data.topks,
+        }
+    }
+}
