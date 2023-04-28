990ca0d701a77897624eae53116a781b399f4009
Xin Liu <sam@secondstate.io>
2023-04-29 01:02:07 +0800

    chore: update example
    Signed-off-by: Xin Liu <sam@secondstate.io>

--- examples/embeddings.rs
+++ examples/embeddings.rs
@@ -1,12 +1,32 @@
+use async_openai::{
+    types::{CreateEmbeddingRequestArgs, Embedding},
+    Client as AIClient,
+};
 use std::io::{self, Write};
 use wasmedge_vdb_sdk::{
     client::Client,
     common::{DataType, Field, FieldData, ScalarField, VectorField},
     schema::{CollectionSchema, FieldSchema, FieldType},
+    utils,
 };
 
 #[tokio::main]
 async fn main() -> Result<(), Box<dyn std::error::Error>> {
+    // ======== step-1: create embeddings for a document with openai api ========
+    print!("[step-1] create embeddings for a document with openai api ... ");
+    io::stdout().flush().unwrap();
+
+    let openai_client = AIClient::new();
+
+    let book_contents = vec![
+        "Why do programmers hate nature? It has too many bugs.",
+        "Why was the computer cold? It left its Windows open.",
+    ];
+    let book_embeddings = gen_embeddings(&openai_client, book_contents).await?;
+
+    println!("Done");
+
+    // ========== step-2: create collection for storing data==========
     print!("[step-2] create collection ... ");
     io::stdout().flush().unwrap();
 
@@ -52,11 +72,14 @@ async fn main() -> Result<(), Box<dyn std::error::Error>> {
 
     println!("Done");
 
-    print!("[step-3] Fill in the collection ... ");
+    // ========== step-2: fill in the collection with data ==========
+    print!("[step-2] Fill in the collection ... ");
     io::stdout().flush().unwrap();
 
+    // psudo embeddings
     let embeddings = vec![0.1_f32, 0.2, 0.3];
 
+    // insert data
     client
         .insert(
             collection_name,
@@ -80,9 +103,67 @@ async fn main() -> Result<(), Box<dyn std::error::Error>> {
 
     println!("Done");
 
+    // ============== step-3: perform a vector query ==============
+    print!("[step-3] perform a vector query ... ");
+    io::stdout().flush().unwrap();
+
+    // generate a vector from a user input text
+    let text = vec!["the windows of the computer are open."];
+    let embeddings = gen_embeddings(&openai_client, text).await?;
+    let query_embedding = embeddings[0].embedding.clone();
+
+    // load the newly created collection to memory before query
+    client.load_collection(collection_name, 1).await?;
+
+    // // perform vector similarity search
+    // let data = vec![];
+    // client.search(
+    //     collection_name,
+    //     vec![],
+    //     "".to_string(),
+    //     data,
+    //     DslType::BoolExprV1,
+    //     vec!["book_intro"],
+    //     std::collections::HashMap::new(),
+    //     0,
+    //     utils::get_gts(),
+    //     data.len() as i64,
+    // );
+
+    // let result = collection
+    //     .search(
+    //         vec![query_embedding.into()],
+    //         "book_intro",
+    //         1,
+    //         MetricType::L2,
+    //         vec!["book_name"],
+    //         &SearchOption::default(),
+    //     )
+    //     .await?;
+
+    // println!("Done");
+
+    // // display the search result
+    // println!("  *** search result: {:?}", result.len());
+    // println!("  *** result[0].field: {:?}", result[0].field);
+
     Ok(())
 }
 
 fn get_vdb_host_address() -> String {
     std::env::var("VDB_HOST").expect("VDB_HOST is not set")
+}
+
+async fn gen_embeddings(
+    client: &AIClient,
+    text: Vec<&str>,
+) -> Result<Vec<Embedding>, Box<dyn std::error::Error>> {
+    let request = CreateEmbeddingRequestArgs::default()
+        .model("text-embedding-ada-002")
+        .input(text)
+        .build()?;
+
+    let response = client.embeddings().create(request).await?;
+
+    Ok(response.data)
 }
